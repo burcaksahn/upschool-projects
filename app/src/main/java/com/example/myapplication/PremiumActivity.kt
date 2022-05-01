975afbcca79ddb8c67fda16e84f1d9aca2ff480d
burcaksahn <burcak.sahn@hotmail.com>
2022-05-01 19:33:47 +0300

    Databinding added.

--- app/src/main/java/com/example/myapplication/PremiumActivity.kt
+++ app/src/main/java/com/example/myapplication/PremiumActivity.kt
@@ -2,10 +2,28 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityPremiumBinding
+
 
 class PremiumActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPremiumBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_premium)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_premium)
+        val model = PremiumModel(
+            "$11.99",
+            "$143.99 per year",
+            "$4.99",
+            "$11.99 per month",
+            "$59.99 per year",
+            "$4.99 per month",
+            "Get unlimited access!",
+            "Monthly",
+            "Yearly"
+        )
+        binding.premium=model
     }
 }
---
+++ app/src/main/java/com/example/myapplication/PremiumModel.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+data class PremiumModel(
+
+val textViewMonth: String,
+val textViewMonthPerYear: String,
+val textViewYearly: String,
+val textViewPerMonth: String,
+val textViewYearlyPerYear: String,
+val textViewPerYearly: String,
+val textViewUnlimited: String,
+val textView17: String, //Soldkai monthlya ait
+val textView18: String,   // sağdaki yeara ait
+)
